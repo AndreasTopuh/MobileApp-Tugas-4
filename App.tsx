8233db21bc2ca6491134fe390fc1e5ef11f1b48e
ezrapelealu <pelealuezra3@gmail.com>
2024-02-19 16:53:00 +0800

    adding register

--- App.tsx
+++ App.tsx
@@ -1,94 +1,13 @@
-import React, {useState} from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  StyleSheet,
-} from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import Register from './component/Registration';
 
-const SignInScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSignIn = () => {
-    // Lakukan proses sign in di sini
-    console.log('Username:', username);
-    console.log('Password:', password);
-  };
-
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcomeText}>Welcome</Text>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Username</Text>
-        <TextInput
-          style={styles.input}
-          value={username}
-          onChangeText={setUsername}
-          placeholder="Masukan username anda"
-        />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Password</Text>
-        <TextInput
-          style={styles.input}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Masukan password anda"
-          secureTextEntry={true}
-        />
-      </View>
-      <TouchableOpacity style={styles.signInButton} onPress={handleSignIn}>
-        <Text style={styles.signInButtonText}>Sign In</Text>
-      </TouchableOpacity>
+    <View style={{ flex: 1 }}>
+      <Register />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    paddingHorizontal: 20,
-    paddingTop: 30,
-  },
-  welcomeText: {
-    color: 'black',
-    fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  inputContainer: {
-    marginBottom: 15,
-    width: '100%',
-  },
-  inputLabel: {
-    marginBottom: 5,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-  },
-  signInButton: {
-    backgroundColor: 'orange',
-    paddingVertical: 16,
-    paddingHorizontal: 30,
-    borderRadius: 10,
-    alignSelf: 'center',
-    width: '100%',
-    marginTop: 30,
-  },
-  signInButtonText: {
-    color: 'white',
-    fontSize: 14,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
-
-export default SignInScreen;
+export default App;
