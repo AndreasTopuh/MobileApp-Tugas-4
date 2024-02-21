ad46e3e50c1d69f8ef65a7e021a0158bbf3d1ff0
AndreasTopuh <andreastopuh23@gmail.com>
2024-02-21 14:32:39 +0800

    edited some files

--- App.tsx
+++ App.tsx
@@ -3,12 +3,11 @@
 import React from 'react';
 import {View} from 'react-native';
 import UserList from './component/Userlist/UserListScreen';
-// import FancyCard from './component/Userlist/FancyCard';
 
 const App = () => {
   return (
     <View>
-      {/* <FancyCard /> */}
+      
       <UserList />
     </View>
   );
